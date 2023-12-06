3d6a26ed948f9cd7589dd2350be134d1735aebfd
sawfiz <dashu.up@gmail.com>
2023-12-06 15:31:12 +0800

    Refactor HighScore component

---
+++ frontend/src/components/HighScore.jsx
@@ -0,0 +1,61 @@
+// Libraries
+import React, { useState, useContext, useEffect } from 'react';
+
+import axios from 'axios';
+import { format } from 'date-fns';
+
+// Vite handles .env differently from create-react-app
+const BASE_URL = import.meta.env.VITE_BASE_URL; // Set the base URL
+
+// Styling
+import { Button, Card } from 'react-bootstrap';
+
+export default function HighScore() {
+  const [scores, setScores] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`${BASE_URL}/scores`);
+        console.log(
+          '🚀 ~ file: Home.jsx:31 ~ fetchData ~ response:',
+          response.data.scores_list
+        );
+        setScores(response.data.scores_list);
+      } catch (error) {
+        console.log('🚀 ~ file: Home.jsx:33 ~ fetchData ~ error:', error);
+        // Handle error
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const scoresList = scores.map((score, index) => (
+    <div key={score._id} className="grid grid-flow-col ">
+      <div style={{ width: '2rem' }}>{index + 1}</div>
+      <div style={{ width: '3rem' }}>{score.time}</div>
+      <div style={{ width: '4rem' }}>{score.name}</div>
+      <div style={{ width: '8rem' }}>
+        {format(new Date(score.date), 'yyyy-MM-dd')}
+      </div>
+    </div>
+  ));
+
+  return (
+    <Card bg="light" style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>High Score</Card.Title>
+        <Card.Text>
+          <div className="grid grid-flow-col ">
+            <div style={{ width: '2rem' }}>No</div>
+            <div style={{ width: '3rem' }}>Time</div>
+            <div style={{ width: '4rem' }}>Name</div>
+            <div style={{ width: '4rem' }}>Date</div>
+          </div>
+          {scoresList}
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,11 +7,12 @@ import { format } from 'date-fns';
 
 import { GameContext } from '../contexts/GameContext';
 
-// Vite handles .env differently from create-react-app
-const BASE_URL = import.meta.env.VITE_BASE_URL; // Set the base URL
+
 
 // Components
 import Game from '../components/Game';
+import HighScore from '../components/HighScore';
+
 
 
 // Background images
@@ -19,42 +20,14 @@ import room from '../assets/images/room.png';
 import beach from '../assets/images/beach.jpg';
 import dragons from '../assets/images/dragons.webp';
 
-// Styling
-import { Button, Card } from 'react-bootstrap';
+
 
 export default function Home() {
   const navigate = useNavigate();
   const { startGame } = useContext(GameContext);
-  const [scores, setScores] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(`${BASE_URL}/scores`);
-        console.log(
-          '🚀 ~ file: Home.jsx:31 ~ fetchData ~ response:',
-          response.data.scores_list
-        );
-        setScores(response.data.scores_list);
-      } catch (error) {
-        console.log('🚀 ~ file: Home.jsx:33 ~ fetchData ~ error:', error);
-        // Handle error
-      }
-    };
 
-    fetchData();
-  }, []);
 
-  const scoresList = scores.map((score, index) => (
-    <div key={score._id} className="grid grid-flow-col ">
-      <div style={{ width: '2rem' }}>{index + 1}</div>
-      <div style={{ width: '3rem' }}>{score.time}</div>
-      <div style={{ width: '4rem' }}>{score.name}</div>
-      <div style={{ width: '8rem' }}>
-        {format(new Date(score.date), 'yyyy-MM-dd')}
-      </div>
-    </div>
-  ));
 
 
 
@@ -65,34 +38,11 @@ export default function Home() {
        <Game gameId={2} imageSrc={beach} />
        <Game gameId={3} imageSrc={dragons} />
       </div>
-      
+
       <div className="mt-20 flex justify-center items-center gap-4">
-        <Card bg="light" style={{ width: '18rem' }}>
-          <Card.Body>
-            <Card.Title>High Score</Card.Title>
-            <Card.Text>
-              <div className="grid grid-flow-col ">
-                <div style={{ width: '2rem' }}>No</div>
-                <div style={{ width: '3rem' }}>Time</div>
-                <div style={{ width: '4rem' }}>Name</div>
-                <div style={{ width: '4rem' }}>Date</div>
-              </div>
-              {scoresList}
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: '18rem' }}>
-          <Card.Body>
-            <Card.Title>High Score</Card.Title>
-            <Card.Text>Beach full of people.</Card.Text>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: '18rem' }}>
-          <Card.Body>
-            <Card.Title>High Score</Card.Title>
-            <Card.Text>Beach full of people.</Card.Text>
-          </Card.Body>
-        </Card>
+        <HighScore />
+        <HighScore />
+        <HighScore />
       </div>
     </main>
   );
